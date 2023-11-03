1a2f525c03ac1ef98d4782139b8874510ef02f35
khanh0601 <89721214+khanh0601@users.noreply.github.com>
2023-11-03 19:12:56 +0700

    delete

--- wp-content/plugins/lmspro/addons/tutor-zoom/zoom-app/README.md
+++
@@ -1,7 +0,0 @@
-# PHP
-
-Zoom REST API's using PHP
-Visit https://zoom.us/developer for more details
-
-## Support
-For any questions or issues, please visit our new Community Support Forum at https://devforum.zoom.us/
--- wp-content/plugins/lmspro/templates/email/email_heading_content.php
+++
@@ -1,8 +0,0 @@
-<div style="margin-bottom: 30px" class="tutor-mr-160">
-	<h6 data-source="email-heading" class="tutor-email-heading">{email_heading}</h6>
-</div>
-
-<div class="tutor-greetings-content">
-	<p class="tutor-email-greetings"><?php _e( 'Dear {user_name},', 'tutor-pro' ); ?></p>
-	<div class="email-user-content" data-source="email-additional-message">{email_message}</div>
-</div>
