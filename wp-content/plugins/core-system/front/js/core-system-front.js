e6f1f8e6efd934c21efe54f65992eab1fca2f29c
trankhanh040147 <tranquangkhanh2k1qv@gmail.com>
2023-12-03 03:21:43 +0700

    up codes

---
+++ wp-content/plugins/core-system/front/js/core-system-front.js
@@ -0,0 +1,5 @@
+const IMAGE_LOADING = {
+    message: '<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>',
+    css: {border: '', padding: 'none', width: '40px', height: '40px'}
+
+}
