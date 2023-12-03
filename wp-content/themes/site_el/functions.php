efb5b505ce125dde5abc26832b6e62c22e71b048
khanh0601 <89721214+khanh0601@users.noreply.github.com>
2023-12-03 16:08:20 +0700

    Update functions.php

--- wp-content/themes/site_el/functions.php
+++ wp-content/themes/site_el/functions.php
@@ -468,7 +468,6 @@ function tutor_course_categories_shortcode($atts) {
                 var swiper = new Swiper('.swiper-container', {
                     slidesPerView: 3,
       spaceBetween: 30,
-      freeMode: true,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
