fb4c945de1a92f1d4f17eb8d21ade29e2d8f3612
khanh0601 <89721214+khanh0601@users.noreply.github.com>
2023-12-05 00:33:04 +0700

    remove catch

--- wp-content/themes/tutorstarter/game_subtract.php
+++ wp-content/themes/tutorstarter/game_subtract.php
@@ -9,7 +9,7 @@ get_header("iframe");
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
  <?php 
 
-    wp_enqueue_style('game_plus_css', get_template_directory_uri() . '/assets/dist/css/game_subtract.css', array(), '1.1', false);
+    wp_enqueue_style('game_plus_css', get_template_directory_uri() . '/assets/dist/css/game_subtract.css', array(), '1.3', false);
 
  ?>
 </head>
